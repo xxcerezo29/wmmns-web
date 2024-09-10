cdf86ae3ac618e91670080f08c54802f5e5dd3ca
xxcerezo29 <vcerezo29@gmail.com>
2024-09-10 16:22:50 +0800

    update api

--- routes/api.php
+++ routes/api.php
@@ -39,17 +39,17 @@ Route::post('/store/device', function(Request $request){
             if($user instanceof Driver){
                 Devices::create([
                     'driver_id' => $user->id,
-                    'token' => $request->token->value
+                    'token' => $request->token
                 ]);
             }else if($user instanceof Resident){
                 Devices::create([
                     'resident_id' => $user->id,
-                    'token' => $request->token->value
+                    'token' => $request->token
                 ]);
             }else if($user instanceof User){
                 Devices::create([
                     'user_id' => $user->id,
-                    'token' => $request->token->value
+                    'token' => $request->token
                 ]);
             }
 
