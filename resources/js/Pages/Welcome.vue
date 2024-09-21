95eb84cbfefb483a45087ca2824062aa09485fd8
xxcerezo29 <vcerezo29@gmail.com>
2024-09-21 21:24:11 +0800

    update

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -120,7 +120,7 @@ onMounted(() => {
                     <h2 class="text-4xl font-bold leading-snug text-gray-700 mb-10 wow fadeInUp" data-wow-delay="1s">
                         Waste Management with Mobile Nofitication System<br class="hidden lg:block">for CENRO</h2>
                     <div class="text-center mb-10 wow fadeInUp">
-                        <a href="#" rel="nofollow" class="btn">Download the Mobile App. Now</a>
+                        <a href="/apk/wmmns-release.apk" rel="nofollow" class="btn">Download the Mobile App. Now</a>
                     </div>
                     <div class="text-center wow fadeInUp" data-wow-delay="1.6s">
                         <img class="img-fluid mx-auto" src="garbage-truck.png" alt="">
